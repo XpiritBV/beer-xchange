3531c959b5f5cda2d5dadff05db43a3d93631626
Geert van der Cruijsen <geert@vdcruijsen.net>
2019-06-25 22:05:14 +0200

    app insights

--- Xpirit.BeerXchange/Program.cs
+++ Xpirit.BeerXchange/Program.cs
@@ -19,6 +19,7 @@ namespace Xpirit.BeerXchange
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
